77aab51ec4245781d4d30149bb36e239f95ed17a
832960 <55299454+832960@users.noreply.github.com>
2020-01-29 11:05:54 -0800

    School

--- School/src/Main.java
+++ School/src/Main.java
@@ -7,20 +7,20 @@ public class Main {
         ArrayList<Teacher> teachers = new ArrayList<>();
         ArrayList<Student> students = new ArrayList<>();
 
-        students.add(0, new Student("Alex", "Chen", 1));
-        students.add(1, new Student("Emily", "Chen", 1));
-        students.add(2, new Student("Tyrone", "Zheng", 12));
-        students.add(3, new Student("Audrey", "Zeng", 5));
-        students.add(4, new Student("Donald", "Tam", 6));
-        students.add(5, new Student("Hubert", "Wolfeschlegelsteinhausenbergerdorff", 2));
-        students.add(6, new Student("Neil", "Tu", 5));
-        students.add(7, new Student("Terry", "Zeng", 2));
-        students.add(8, new Student("Emily", "Watson", 10));
-        students.add(9, new Student("Walter", "Chen", 3));
+        students.add(new Student("Alex", "Chen", 1));
+        students.add(new Student("Emily", "Chen", 1));
+        students.add(new Student("Tyrone", "Zheng", 12));
+        students.add(new Student("Audrey", "Zeng", 5));
+        students.add(new Student("Donald", "Tam", 6));
+        students.add(new Student("Hubert", "Wolfeschlegelsteinhausenbergerdorff", 2));
+        students.add(new Student("Neil", "Tu", 5));
+        students.add(new Student("Terry", "Zeng", 2));
+        students.add(new Student("Emily", "Watson", 10));
+        students.add(new Student("Walter", "Chen", 3));
 
-        teachers.add(0, new Teacher("Paul", "Pham", "Science"));
-        teachers.add(1, new Teacher("Mark", "Panaligan", "PHE"));
-        teachers.add(2, new Teacher("Alex", "Zeng", "Mandarin"));
+        teachers.add(new Teacher("Paul", "Pham", "Science"));
+        teachers.add(new Teacher("Mark", "Panaligan", "PHE"));
+        teachers.add(new Teacher("Alex", "Zeng", "Mandarin"));
 
         School school = new School("Windermere", 30, 20, teachers, students);
 

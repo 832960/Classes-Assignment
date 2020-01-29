f74cb8ea6bae50ad72aa8d69cfa69b70bd0efe18
832960 <55299454+832960@users.noreply.github.com>
2020-01-29 11:20:55 -0800

    Time

--- Time/src/Main.java
+++ Time/src/Main.java
@@ -26,9 +26,6 @@ public class Main {
         time.setHour(43);
         System.out.println(time);
 
-        System.out.println("Next Second");
-        time.nextSecond();
-        System.out.println(time);
 
     }
 
